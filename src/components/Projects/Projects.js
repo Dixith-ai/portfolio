6623e3d82f796d9ef3426d025a340e61a179b570
DIXITH ADITHYA <31dixithadithya@gmail.com>
2024-07-10 01:28:01 +0530

    changes made in projects and certifications

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import leaf from "../../Assets/Projects/coming-soon.jpg";
+import emotion from "../../Assets/Projects/coming-soon.jpg";
+import editor from "../../Assets/Projects/coming-soon.jpg";
+import chatify from "../../Assets/Projects/coming-soon.jpg";
+import suicide from "../../Assets/Projects/coming-soon.jpg";
+import bitsOfCode from "../../Assets/Projects/coming-soon.jpg";
 
 function Projects() {
   return (
